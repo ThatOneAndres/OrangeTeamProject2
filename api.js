f982c0a009545488427ad6683485d72abaa8b552
Andres Valdovinos <avaldov1@uci.edu>
2017-10-01 18:12:29 -0700

    Added throw for foods

--- api.js
+++ api.js
@@ -26,34 +26,38 @@ request(constructURL(obj2), function (error, response, body) {
   });
 
 function constructURL(paramObject){
-    var queryURL = "http://api.edamam.com/search?app_id=f2b0bd06&app_key=47f2733377e5a86b25e92d25a29fb72a&q=";
-    var foods = paramObject.foods.toString();
-    queryURL += foods;
-    if (paramObject.calories){
-        if(typeof paramObject.calories !== "number"){
-            throw "calorie needs to be an integer";
-        }else{
-            queryURL += "&calories=";
-            queryURL += "lte " + paramObject.calories;
+    if (paramObject.foods){
+        var queryURL = "http://api.edamam.com/search?app_id=f2b0bd06&app_key=47f2733377e5a86b25e92d25a29fb72a&q=";
+        var foods = paramObject.foods.toString();
+        queryURL += foods;
+        if (paramObject.calories){
+            if(typeof paramObject.calories !== "number"){
+                throw "calorie needs to be an integer";
+            }else{
+                queryURL += "&calories=";
+                queryURL += "lte " + paramObject.calories;
+            }
         }
-    }
-    if (paramObject.health){
-        if (typeof paramObject.health !== "string"){
-            throw "health needs to be a String";
-        }else{
-            queryURL += "&health=";
-            queryURL += paramObject.health;
+        if (paramObject.health){
+            if (typeof paramObject.health !== "string"){
+                throw "health needs to be a String";
+            }else{
+                queryURL += "&health=";
+                queryURL += paramObject.health;
+            }
         }
-    }
-    if (paramObject.recipeCount){
-        if (typeof paramObject.recipeCount !== "number"){
-            throw "recipeCount needs to be an integer";
-        }else{
-            queryURL += "&to=";
-            queryURL += paramObject.recipeCount;
+        if (paramObject.recipeCount){
+            if (typeof paramObject.recipeCount !== "number"){
+                throw "recipeCount needs to be an integer";
+            }else{
+                queryURL += "&to=";
+                queryURL += paramObject.recipeCount;
+            }
         }
+        return queryURL;
+    }else{
+        throw "foods property is required"
     }
-    return queryURL;
 }
 
 console.log("obj1: ",constructURL(obj1));
