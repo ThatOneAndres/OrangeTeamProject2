a919a4467dbd24a9cc5e25405e5221296e800d0c
Om Kapoor <omkapoor@Oms-MacBook-Pro-2.local>
2017-10-03 20:06:01 -0700

    added login functionality

--- routes/login-api-route.js
+++ routes/login-api-route.js
@@ -1,39 +1,41 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
 var db = require("../models");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
-    // 1. Add a join to include all of each Author's Posts
-    db.Author.findAll({include: db.Post}).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
+
+  // Registers new user into DataBase
+  app.post("/api/registeruser", function(req, res) {
+    // Add sequelize code to find all posts, and return them to the user with res.json
+    console.log(req)
+    console.log(req.body.username);
+    console.log(req.body.password)
+    db.users.create({
+      username: req.body.username,
+      password: req.body.password,
+    }).then((result) => {
+      res.json(result);
+    })
   });
 
-  app.get("/api/authors/:id", function(req, res) {
-    // 2; Add a join to include all of the Author's Posts here
-    db.Author.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: db.Post
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
+  // Validates if user auth is valid
+  app.post("/api/registeruser", function(req, res) {
+    // Add sequelize code for creating a post using req.body,
+    // then return the result using res.json
+    db.users.findAll({where: {
+      username: req.body.username,
+      password: req.body.password
+    }}).then((result) => {
+      res.json (result);
+      console.log(result)
+    })
   });
-
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
-};
+}
