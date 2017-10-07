15a209e4620be823b192a60ff7bc589fae9a2e8e
Om Kapoor <omkapoor@dhcp-v073-046.mobile.uci.edu>
2017-10-07 15:07:01 -0700

    functional styled recipe cards

--- public/assets/js/request.js
+++ public/assets/js/request.js
@@ -1,43 +1,118 @@
 $(document).ready(function(){
 
-    $("button").click(function(){
+    $("#searchRecipeButton").click(function(){
         var foods = {
           foods : [],
           health : "peanut-free",
           recipeCount: 10,
           calories: 1000
         };
-        var x = $("form").serializeArray();
-        console.log(x);
+        var searchedRecipe = $("#searchRecipe").val()
 
-        for(var i = 0 ; i < x.length; i++){
-          foods.foods.push(x[i].name);
-        }
-      console.log(foods);
+        console.log(searchedRecipe);
+       foods.foods = searchedRecipe.split(" ");
+        console.log(foods);
 
         $.post("api/recipesearch/", foods)
           .done(function(result){
 
-             console.log(result);
+            console.log(result);
+
+            var row = $("<row>");
+            $("#results").append(row);
 
              for(var i = 0; i < result.length; i++){
-               var card = '<div class="col-md-4 col-sm-6 item"><div class="card"><h4 class="text-center">';
+              //Define recipe Item with props
+              var recipeItem = result[i].recipe;
 
-               card += result[i].recipe.label;
+              //Define Jquery Dom elements
+              var favButton = $("<a>")
+              var colDiv = $("<div>");
+              var cardDiv = $("<div>");
+              var cardBlock = $("<div>");
+              var title = $("<h4>");
+              var img = $("<img>");
+              var dietLabels = $("<p>");
+              var fullRecipe = $("<a>");
 
-               card += '</h4><img class="card-img-top" src="';
+              //Add attributes to elements
+              favButton.attr({
+                name: recipeItem.label,
+                class: "favButton",
+              });
 
-               card += result[i].recipe.image;
+              colDiv.attr({
+                class: "col-md-4 col-sm-6",
+              });
 
-               card += '" alt="Card image cap"><ul class="nav nav-tabs"><li class="nav-item"><a class="nav-link active" href="#">Recipe</a></li><li class="nav-item">';
-               card += '<a class="nav-link" href="#">Nutrition</a></li><li class="nav-item"><a class="nav-link disabled" href="#">Diet</a></li></ul><div class="card-body"><p class="card-text">';
+              cardDiv.attr({
+                class: "card recipeCard",
+              });
+
+              img.attr({
+                src: recipeItem.image,
+                alt: recipeItem.label,
+                class: "card-img-top",
+                style: "width:300px;height:300px;text-align:center;border-radius:3px"
+              })
+
+              cardBlock.attr({
+                class: "card-block",
+                style: "text-align:center"
+              });
+
+              title.attr({
+                class: "card-title",
+                style: "font-weight: bold;"
+              })
+
+              title.text(recipeItem.label);
 
 
-               card += result[i].recipe.label;
+              dietLabels.attr({
+                style: "font-weight:bold;color:black"
+              })
 
-               card += '</p></div></div></div>';
+              console.log(recipeItem.dietLabels);
+              dietLabels.text("Diet Labels : " + recipeItem.dietLabels.toString());
 
-                $("#results").append(card);
+              fullRecipe.attr({
+                href: recipeItem.url,
+                class: "btn btn-primary",
+                target: "_blank"
+              });
+
+              fullRecipe.text("Full Recipe")
+
+              colDiv.append(cardDiv);
+              cardDiv.append(img);
+              cardDiv.append($("<hr>"))
+              cardBlock.append(title);
+              cardBlock.append($("<hr>"));
+              cardBlock.append(dietLabels);
+              cardBlock.append($("<hr>"));
+              cardBlock.append(fullRecipe);
+
+
+              cardDiv.append(cardBlock)
+
+               // var card = '<div class="col-md-4 col-sm-6 item"><div class="card"><h4 class="text-center">';
+
+               // card += result[i].recipe.label;
+
+               // card += '</h4><img class="card-img-top" src="';
+
+               // card += result[i].recipe.image;
+
+               // card += '" alt="Card image cap"><ul class="nav nav-tabs"><li class="nav-item"><a class="nav-link active" href="#">Recipe</a></li><li class="nav-item">';
+               // card += '<a class="nav-link" href="#">Nutrition</a></li><li class="nav-item"><a class="nav-link disabled" href="#">Diet</a></li></ul><div class="card-body"><p class="card-text">';
+
+
+               // card += result[i].recipe.label;
+
+               // card += '</p></div></div></div>';
+
+                $("#results").append(colDiv);
              }
 
           }
