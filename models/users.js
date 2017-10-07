142db3db82919b0b869fb3b0467b35cae0a090c9
Andres Valdovinos <avaldov1@uci.edu>
2017-10-06 23:44:28 -0700

    Added multiple joins

--- models/users.js
+++ models/users.js
@@ -19,18 +19,17 @@ module.exports = function (sequelize, DataTypes) {
 		}
 	});
 
-	// users.associate = function(models) {
-	// 	// Associating Author with Posts
-	// 	// When an Author is deleted, also delete any associated Posts
-	// 	users.hasMany(models.Ingredient, {
-	// 	  onDelete: "cascade"
-	// 	});
-
-	// 	users.hasMany(models.favorites, {
-	// 		onDelete: "cascade"
-	// 	});
+	users.associate = function(models) {
+		// Associating Author with Posts
+		// When an Author is deleted, also delete any associated Posts
+		users.hasMany(models.Ingredients, {
+		  onDelete: "cascade"
+		});
+		users.hasMany(models.favorites, {
+			onDelete: "cascade"
+		});
 		
-	// };
+	};
 
 	return users;
 }
