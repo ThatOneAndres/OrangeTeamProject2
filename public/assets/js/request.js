9b22c4160fb1b5746017bcf26109eae3a0a24251
Om Kapoor <omkapoor@dhcp-v122-017.mobile.uci.edu>
2017-10-10 20:01:24 -0700

    updates to navbar

--- public/assets/js/request.js
+++ public/assets/js/request.js
@@ -79,29 +79,12 @@ function displayRecipes(result){
   
                 cardDiv.append(cardBlock)
   
-                 // var card = '<div class="col-md-4 col-sm-6 item"><div class="card"><h4 class="text-center">';
-  
-                 // card += result[i].recipe.label;
-  
-                 // card += '</h4><img class="card-img-top" src="';
-  
-                 // card += result[i].recipe.image;
-  
-                 // card += '" alt="Card image cap"><ul class="nav nav-tabs"><li class="nav-item"><a class="nav-link active" href="#">Recipe</a></li><li class="nav-item">';
-                 // card += '<a class="nav-link" href="#">Nutrition</a></li><li class="nav-item"><a class="nav-link disabled" href="#">Diet</a></li></ul><div class="card-body"><p class="card-text">';
-  
-  
-                 // card += result[i].recipe.label;
-  
-                 // card += '</p></div></div></div>';
-  
                   $("#results").append(colDiv);
                }
   
             };
 
 $(document).ready(function(){
-
     $("#searchRecipeButton").click(function(){
       event.preventDefault();
       $("#results").empty();
